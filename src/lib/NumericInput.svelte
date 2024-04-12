f39c2735569dc5cb24726f352a5a0536de9c1680
Max Loiacono <max.loiacono@protonmail.com>
2024-04-12 14:44:47 -0400

    input validation

--- src/lib/NumericInput.svelte
+++ src/lib/NumericInput.svelte
@@ -1,14 +1,37 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+
 	export let title = "Input";
 	export let placeholder = "";
 	export let help="";
+	let text = "";
+	const dispatch = createEventDispatcher();
+
+	function validate(){
+		if(text == "."){
+			text = "0.";
+		}
+
+		var value = parseFloat(text);
+
+		if(value >= 0 && value != undefined){
+			//
+		}
+		else{
+			text = "";
+		}
+
+		dispatch("validInput", {
+			text: text
+		});
+	}
 </script>
 
 <div class="field">
 	<!-- svelte-ignore a11y-label-has-associated-control -->
 	<label class="label">{title}</label>
 	<div class="control">
-		<input class="input" type="number" min=0 placeholder="{ placeholder ? placeholder : title}">
+		<input class="input" type="text" min=0 placeholder="{ placeholder ? placeholder : title}" bind:value={text} on:input={validate}>
 	</div>
 	<p class="help" hidden="{help == ""}">{help}</p>
 </div>
